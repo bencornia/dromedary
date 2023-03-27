e3939bf3784816e8e0a150bcb1ff831a53798591
bencornia <ben.cornia@gmail.com>
2023-03-27 13:33:16 -0600

    Split backend into its own project.

--- backend/controllers/not-found.controller.js
+++
@@ -1,5 +0,0 @@
-function notFound(req, res) {
-    res.status(404).send({ message: "page not found" });
-}
-
-module.exports = { notFound };
--- backend/models/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const { config } = require("../config/config");
-
-async function connectDB() {
-    await mongoose.connect(config.connectionString, { dbName: "dromedarydb" });
-    console.log("[ database ]: connected...");
-}
-
-module.exports = { connectDB };
